729741c5b1f54b6c8d9f49904e4b9cefdcbb6bd4
RyanDur <ryan.p.durling@gmail.com>
2019-10-29 13:55:23 -0500

    more

--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -1,14 +1,14 @@
 package test.pivotal.pal.tracker;
 
 import io.pivotal.pal.tracker.WelcomeController;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class WelcomeControllerTest {
+class WelcomeControllerTest {
 
     @Test
-    public void itSaysHello() throws Exception {
+    void itSaysHello() {
         WelcomeController controller = new WelcomeController("A welcome message");
 
         assertThat(controller.sayHello()).isEqualTo("A welcome message");
